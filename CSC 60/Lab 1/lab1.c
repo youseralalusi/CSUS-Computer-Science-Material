f21b2b1c8be11f1e043bc4b22a2badd32500f283
Youser Alalusi <79121480+youseralalusi@users.noreply.github.com>
2021-03-01 13:43:17 -0800

    Add files via upload

---
+++ CSC 60/Lab 1/lab1.c
@@ -0,0 +1,17 @@
+/*----------------*/
+/* Youser Alalusi */
+/* Lab 1 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+	printf("\nLab 1. \n\n");
+	printf("Hello World.\n");
+	printf("Hi, Youser Alalusi. \n\n");
+	printf("To appreciate the sun you gotta know what rain is. -J.Cole\n\n");
+	
+	return EXIT_SUCCESS;
+}
+/*----------------*/
+
