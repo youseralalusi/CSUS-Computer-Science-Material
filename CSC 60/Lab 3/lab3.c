eec140cd36faa832ed5a81a708801348d5605aa9
Youser Alalusi <79121480+youseralalusi@users.noreply.github.com>
2021-03-01 21:10:06 -0800

    Add files via upload

---
+++ CSC 60/Lab 3/lab3.c
@@ -0,0 +1,27 @@
+/*-----------------*/
+/* Youser Alalusi  */
+/* Lab 3           */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int age = 22;
+	double average = 416.67234;
+
+	printf("\nYouser Alalusi. Lab 3.\n");
+	printf("\nMary went to the store.\n");
+	printf("\nMy friend is %d years old.\n", age);
+	printf("\nSo many books, so little time.\"");
+	printf("\n- Frank Zappa\n");
+	printf("\nThe average = %.1f\n", average);
+	printf("\nThe average = %.3f\n", average);
+	printf("\nThe average = %-.2e\n", average);
+	printf("\nThe average = %f\n", average);
+	printf("\n");
+	
+	return EXIT_SUCCESS;
+}
+
+	
